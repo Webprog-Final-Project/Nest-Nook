92c54e08e2110a0650c015c721f1536e56339156
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-19 16:09:40 -0400

    Added verify user and insert user functions

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
+
+/*--- Connecting and disconnecting to the database ---*/
+
 function openConnection($servername, $username, $password, $dbname) {
+
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,5 +15,56 @@ function openConnection($servername, $username, $password, $dbname) {
 }
 function closeConnection($conn) {
 	$conn->close();
+}
+
+/*--- Verify the new user from Signup page is not already in the DB ---*/
+
+function verifyUniqueUser($conn, $email) {
+	// Prepare and bind SQL statement
+	$select_query = $conn->prepare(
+		"SELECT email FROM users WHERE email = ?"
+	);
+	$select_query->bind_param("s", $email);
+
+	// Execute SQL statement
+	if ($select_query->execute()) {
+		$select_query->store_result();
+
+		// If email is unique: return true
+		if ($select_query->num_rows === 0) {
+			$select_query->close();
+			return true;
+		}
+		// If not: return false
+		else {
+			$select_query->close();
+			return false;
+		}
+	}
+	else {
+		$select_query->close();
+		return false;
+	}
+}
+
+/*--- Add new user from Signup page after verifying ---*/
+
+function addNewUser($conn, $first_name, $last_name, $email, $password) {
+	// Prepare and bind SQL statement
+	$insert_query = $conn->prepare(
+		"INSERT INTO users (first_name, last_name, email, password, role)
+		VALUES (?, ?, ?, ?, 'Seller')"
+	);
+	$insert_query->bind_param("ssss", $first_name, $last_name, $email, $password);
+
+	// Execute SQL statement
+	if ($insert_query->execute()) {
+		header("location:Homepage.html");
+		exit;
+	}
+	else {
+		echo "Error: " . $insert_query->error;
+	}
+	$insert_query->close();
 }
 ?>
