3fa9420f75892c3d302e4896fb69a63424db6e55
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-19 16:08:39 -0400

    Added VerifyUniqueUser() and refactored code

--- process_signup.php
+++ process_signup.php
@@ -8,28 +8,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"
 	&& !empty($_POST['email'])
 	&& !empty($_POST['password'])
 	) {
-	
-	// Prepare and bind SQL statement
-	$insert_query = $conn->prepare(
-		"INSERT INTO users (first_name, last_name, email, password, role)
-		VALUES (?, ?, ?, ?, 'Seller')" 
-	);
-	$insert_query->bind_param("ssss", $first_name, $last_name, $email, $password);
-
-	// Retrieve form data
 	$first_name = $_POST['first_name'];
 	$last_name = $_POST['last_name'];
 	$email = $_POST['email'];
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-	// Execute SQL statement
-	if ($insert_query->execute()) {
-		header("location:Homepage.html");
-		exit;
+	if (verifyUniqueUser($conn, $email)) {
+		addNewUser($conn,$first_name, $last_name, $email, $password);
 	} else {
-    		echo "Error: " . $insert_query->error;
+		// User already registered
 	}
-	$insert_query->close();
 }
 closeConnection($conn);
 ?>
