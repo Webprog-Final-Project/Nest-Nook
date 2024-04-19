096102156260b39610b6e29d47ac3d0d6b856845
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-19 06:25:31 -0400

    Upload Signup files

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function openConnection($servername, $username, $password, $dbname) {
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	return $conn;
+}
+function closeConnection($conn) {
+	$conn->close();
+}
+?>
---
+++ process_signup.php
@@ -0,0 +1,39 @@
+<?php require('functions.php');
+
+$conn = openConnection("localhost", "lkinsey2", "lkinsey2", "lkinsey2");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"
+	&& !empty($_POST['first_name'])
+	&& !empty($_POST['last_name'])
+	&& !empty($_POST['email'])
+	&& !empty($_POST['password'])
+	) {
+	
+	// Prepare and bind SQL statement
+	$insert_query = $conn->prepare(
+		"INSERT INTO users (first_name, last_name, email, password, role)
+		VALUES (?, ?, ?, ?, 'Seller')"
+	);
+	$insert_query->bind_param("ssss", $first_name, $last_name, $email, $password);
+
+	// Retrieve form data
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	// Execute SQL statement
+	if ($insert_query->execute()) {
+		header("location:Homepage.html");
+		exit;
+	} else {
+    		echo "Error: " . $insert_query->error;
+	}
+	$insert_query->close();
+} else {
+	
+	// Error handling
+	
+}
+closeConnection($conn);
+?>
