4a33aa1c0efb74f1dc185c22fd5955d66e4f1697
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-19 23:12:14 -0400

    correctly compares passwords

--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ function verifyUniqueUser($conn, $email) {
 /*--- Add new user from Signup page after verifying ---*/
 
 function addNewUser($conn, $first_name, $last_name, $email, $password) {
+
 	// Prepare and bind SQL statement
 	$insert_query = $conn->prepare(
 		"INSERT INTO users (first_name, last_name, email, password, role)
@@ -71,22 +72,34 @@ function addNewUser($conn, $first_name, $last_name, $email, $password) {
 /*--- Login: Check if credentials correct / User is in the Database ---*/
 
 function findUser($conn, $email, $password) {
+    $db_email = '';
+    $db_password = '';
 	// Prepare and bind SQL statement
 	$select_query = $conn->prepare(
-		"SELECT email, password FROM users WHERE email = ? AND password = ?"
+		"SELECT email, password FROM users WHERE email = ?"
 	);
-	$select_query->bind_param("ss", $email, $password);
+	$select_query->bind_param("s", $email);
 
 	// Execute SQL statement
 	if ($select_query->execute()) {
 		$select_query->store_result();
 
-		// If credentials are correct / found: return true
+		// If credentials are found
 		if ($select_query->num_rows === 1) {
-			$select_query->close();
-			return true;
+
+            // Assign query result to variables
+            $select_query->bind_result($db_email, $db_password);
+            $select_query->fetch();
+            $select_query->close();
+
+            // Compare password hashes
+            if (password_verify($password, $db_password)) {
+                return true;
+            }
+            else {
+                return false;
+            }
 		}
-		// If not: return false
 		else {
 			$select_query->close();
 			return false;
