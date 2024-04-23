ef44c94c74e003f3a676cebc5dd22746fe26140f
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-23 14:35:51 -0400

    Update seller_dashboard.php

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -1,3 +1,16 @@
 <?php require ('functions.php');
 session_start();
 echo "Welcome " . $_SESSION['first_name'] . " " . $_SESSION['last_name'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+</head>
+<body>
+<h1>Seller Dashboard</h1>
+<form action = "new_property.php" method="post">
+    <input type="submit" value="+">
+</form>
+</body>
