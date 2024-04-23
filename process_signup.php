7af309e7723d6acde0867d679797dc497adef93b
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-22 21:06:00 -0400

    Include Role from signup.php

--- process_signup.php
+++ process_signup.php
@@ -7,14 +7,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"
 	&& !empty($_POST['last_name'])
 	&& !empty($_POST['email'])
 	&& !empty($_POST['password'])
+	&& !empty($_POST['role'])
 	) {
 	$first_name = $_POST['first_name'];
 	$last_name = $_POST['last_name'];
 	$email = $_POST['email'];
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$role = $_POST['role'];
 
 	if (verifyUniqueUser($conn, $email) && verifyEmailFormat($email)) {
-		addNewUser($conn,$first_name, $last_name, $email, $password);
+		addNewUser($conn,$first_name, $last_name, $email, $password, $role);
 	} else {
 		// Redirect the user to the signup page with an error parameter
 		header("Location: signup.php?error=2");
