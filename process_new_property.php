83fec2ec338684958185a41a03167a6d8c6017bd
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-24 16:47:36 -0400

    Can now upload pics for property

--- process_new_property.php
+++ process_new_property.php
@@ -16,16 +16,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"
     $baths = $_POST['baths'];
     $sqft = $_POST['sqft'];
     $year_built = $_POST['year_built'];
-    $interior = file_get_contents($_FILES['interior']['tmp_name']);
-    $interior = $conn->real_escape_string($interior);
+
     $exterior = file_get_contents($_FILES['exterior']['tmp_name']);
-    $exterior = $conn->real_escape_string($exterior);
+    $exterior_file_type = getImageType($_FILES['exterior']['tmp_name']);
+
+    $interior = file_get_contents($_FILES['interior']['tmp_name']);
+    $interior_file_type = getImageType($_FILES['interior']['tmp_name']);
+
     $description = $_POST['description'];
     $date_listed = date("Y-m-d");
 
-
     if (addProperty($conn, $user_id, $property_id, $price, $beds, $baths, $sqft, $description, $address, $residence_type, $year_built, $date_listed)
-            && addPictures($conn, $property_id, $exterior, $interior)) {
+            && addPictures($conn, $property_id, $exterior, $exterior_file_type, $interior, $interior_file_type)) {
         //Redirect on success
         header('location: seller_dashboard.php?message=Data saved successfully');
         exit;
