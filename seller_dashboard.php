fe1212293994beef86fa591badd6616ffe6a394b
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-23 22:54:46 -0400

    Shows message on property creation
    Mainly for testing for now.

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -1,6 +1,8 @@
 <?php require ('functions.php');
 session_start();
 echo "Welcome " . $_SESSION['first_name'] . " " . $_SESSION['last_name'];
+
+$message = isset($_GET['message']) ? $_GET['message'] : null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,8 +11,18 @@ echo "Welcome " . $_SESSION['first_name'] . " " . $_SESSION['last_name'];
     <title>Dashboard</title>
 </head>
 <body>
-<h1>Seller Dashboard</h1>
-<form action = "new_property.php" method="post">
-    <input type="submit" value="+">
-</form>
+    <h1>Seller Dashboard</h1>
+    <form action = "new_property.php" method="post">
+        <input type="submit" value="+">
+    </form>
+    <div>
+        <?php if ($message) { ?>
+            <p><?=$message?></p>
+        <?php } ?>
+    </div>
+    <div>
+        <?php /*if ($message) { ?>
+                <?=$message?>
+        <?php } */?>
+    </div>
 </body>
