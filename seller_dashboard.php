a14dc73ffecb3598f744864ca05a6ff00c7d89d2
linkon01 <137330762+linkon01@users.noreply.github.com>
2024-04-26 21:01:57 -0400

    Added Notifications for CRUD

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -8,9 +8,18 @@ $user_id = $_SESSION['user_id'];
 // Welcome message
 echo "Welcome " . $_SESSION['first_name'] . " " . $_SESSION['last_name'];
 
-// Message to display when new property is added
+// Confirmation for update, delete, add
 $message = isset($_GET['message']) ? $_GET['message'] : null;
+// Error for update, delete, add
+$error = isset($_GET['error']) ? $_GET['error'] : null;
 
+// reset the message and error displays when confirmed is clicked
+if (isset($_POST['confirm'])) {
+    $message = null;
+    $error = null;
+    header('Location:seller_dashboard.php');
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,14 +29,37 @@ $message = isset($_GET['message']) ? $_GET['message'] : null;
 </head>
 <body>
     <h1>Seller Dashboard</h1>
+
+    <!--- Confirmation for update, delete, add --->
+    <?php if ($message) { ?>
+        <div class="message">
+            <div>
+                <?= displayMessage($message) ?>
+            </div>
+            <form action="seller_dashboard.php" method="post">
+                <input type="submit" value="Confirm">
+            </form>
+        </div>
+    <?php } ?>
+
+    <!--- Error for update, delete, add --->
+    <?php if ($error) { ?>
+        <div class="error_message">
+            <div>
+                <?= displayError($error) ?>
+            </div>
+            <form action="seller_dashboard.php" method="post">
+                <input type="submit" value="Confirm">
+            </form>
+        </div>
+    <?php } ?>
+
+    <!--- Add new property button --->
     <form action = "new_property.php" method="post">
         <input type="submit" value="+">
     </form>
-    <div>
-        <?php if ($message) { ?>
-            <p><?=$message?></p>
-        <?php } ?>
-    </div>
+
+    <!--- Display all properties --->
     <div>
         <?php displayPropertyCards($conn, $user_id); ?>
     </div>
